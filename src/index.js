2055fbaeb2d1e5e5d1ea4715a997f5d073494a41
weiwei <alwaysvailw@live.com>
2020-08-31 16:17:17 +0800

    fix: remove lodash

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ function component() {
   var element = document.createElement('div');
   var element = document.createElement('pre');
 
-  // lodash 是由当前 script 脚本 import 导入进来的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.innerHTML = [
     'Hello webpack!',
     '5 cubed is equal to ' + cube(5)
