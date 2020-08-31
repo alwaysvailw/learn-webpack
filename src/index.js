83a0edf71792468677de6ffab647437ea4cf7136
weiwei <alwaysvailw@live.com>
2020-08-31 16:30:15 +0800

    5.生产环境构建
    - webpack-merge 合并通用配置和分离配置
    - NPM Scripts
    
        ```json
        // package.json
        "scripts": {
        -     "start": "webpack-dev-server --open",
        +     "start": "webpack-dev-server --open --config webpack.dev.js",
        -     "build": "webpack"
        +     "build": "webpack --config webpack.prod.js"
            },
        ```

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import { cube } from './math.js';
 
+if (process.env.NODE_ENV !== 'production') {
+  console.log(process.env.NODE_ENV)
+  console.log('Looks like we are in development mode!');
+}
+
 function component() {
   var element = document.createElement('div');
   var element = document.createElement('pre');
--- webpack.common.js
+++ webpack.common.js
@@ -1,25 +1,19 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: './src/index.js',
-    print: './src/print.js'
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist'
+    app: './src/index.js'
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Output Management'
+      title: 'Production'
     })
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  },
-  mode: "production"
+  }
 };
