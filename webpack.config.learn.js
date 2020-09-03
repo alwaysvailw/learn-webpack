d12a9465c688066238ae7df0e3a8da58693a6806
weiwei <alwaysvailw@live.com>
2020-09-03 17:50:39 +0800

    3.0.1 更新config.learn文件

--- webpack.config.learn.js
+++ webpack.config.learn.js
@@ -1,13 +1,17 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js',
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
+      // 加载CSS
       {
         test: /\.css$/,
         use: [
@@ -15,16 +19,34 @@ module.exports = {
           'css-loader',
         ],
       },
+
+      // 加载图片
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
           'file-loader',
         ],
       },
+
+      // 加载字体
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
           'file-loader',
+        ],
+      },
+
+      // 加载数据
+      {
+        test: /\.(csv|tsv)$/,
+        use: [
+          'csv-loader',
+        ],
+      },
+      {
+        test: /\.xml$/,
+        use: [
+          'xml-loader',
         ],
       },
     ],
